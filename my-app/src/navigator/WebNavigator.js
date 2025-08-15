83e89ee73549fcccdfd5cecb0684a4ea4eae70a6
IamAlonKenneth <jkpalon@mymail.mapua.edu.ph>
2025-08-15 20:14:49 +0800

    added header component

--- my-app/src/navigator/WebNavigator.js
+++ my-app/src/navigator/WebNavigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomeScreen from "../screens/HomeScreen";
-import CartScreen from "../screens/CartScreen";
+import HomeScreen from "../pages/HomeScreen";
+import CartScreen from "../pages/CartScreen";
 
 // Example: Modular route config for web-specific routes
 const webRoutes = [
