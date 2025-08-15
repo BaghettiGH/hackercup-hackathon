9a5901308c047594f7f7c1d0afb0a3bb62251de3
BaghettiGH <patrickmolina042@gmail.com>
2025-08-16 03:32:25 +0800

    ww

--- my-app/src/components/CartRow.jsx
+++ my-app/src/components/CartRow.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Trash2 } from 'lucide-react';
+import './App.css';
 export default function CartRow({productName, supplierName, quantity, totalPrice, productImage}) {
 
 
--- my-app/src/pages/CartScreen.jsx
+++ my-app/src/pages/CartScreen.jsx
@@ -24,12 +24,10 @@ export default function CartScreen() {
 
     useEffect(() =>{
         if(!buyerId) return;
-
         fetchCart();
     },[buyerId]);
 
     async function fetchCart() {
-        // 1. Get orders for the logged-in buyer
         const { data: orders, error: ordersError } = await supabase
             .from('orders')
             .select(`
@@ -39,29 +37,45 @@ export default function CartScreen() {
                     quantity,
                     total_price,
                     product_id (
+                    id,
                         name,
                         product_image,
-                        supplier_id (
-                            name
-                        )
+                        supplier_id 
                     )
                 )
             `)
-            .eq('buyer_id', buyerId);
+            .eq('buyer_id', buyerId)
+            .eq('status','pending');
 
         if (ordersError) {
             console.error(ordersError);
             return;
         }
+        if (!orders || orders.length === 0) {
+            setCartItems([]);
+            return;
+        }
+        
 
+        const supplierIds = [...new Set(
+            orders.map(o => o.order_item_id.product_id.supplier_id)
+        )];
+        const { data: suppliers } = await supabase
+            .from('suppliers')
+            .select('id, name')
+            .in('id', supplierIds);
+
+        const supplierMap = {};
+        suppliers.forEach(s => supplierMap[s.id] = s.name);
         const items = orders.map(order => {
             const item = order.order_item_id;
+            const product = item.product_id;    
             return {
-                productName: item.product_id.name,
-                supplierName: item.product_id.supplier_id.name,
+                productName: product.name,
+                supplierName: supplierMap[product.supplier_id] || 'Unknown Supplier',
                 quantity: item.quantity,
                 totalPrice: item.total_price,
-                productImage: item.product_id.product_image
+                productImage: product.product_image
             };
         });
 
